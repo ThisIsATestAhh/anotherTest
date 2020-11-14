db2dac6d23f688d1d015577dabb91feb69c95f27
Jennelle <43226913+jenoodly@users.noreply.github.com>
2020-11-14 04:00:45 -0800

    test

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,4 @@
+//background animation
 const root = document.documentElement;
 const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
 const marqueeContent = document.querySelector("ul.marquee-content");
@@ -7,3 +8,28 @@ root.style.setProperty("--marquee-elements", marqueeContent.children.length);
 for(let i=0; i<marqueeElementsDisplayed; i++) {
   marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
 }
+
+//navbar
+const themeMap = {
+    dark: "light",
+    light: "solar",
+    solar: "dark"
+  };
+  
+  const theme = localStorage.getItem('theme')
+    || (tmp = Object.keys(themeMap)[0],
+        localStorage.setItem('theme', tmp),
+        tmp);
+  const bodyClass = document.body.classList;
+  bodyClass.add(theme);
+  
+  function toggleTheme() {
+    const current = localStorage.getItem('theme');
+    const next = themeMap[current];
+  
+    bodyClass.replace(current, next);
+    localStorage.setItem('theme', next);
+  }
+  
+  document.getElementById('themeButton').onclick = toggleTheme;
+  
